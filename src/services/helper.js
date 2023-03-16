deba5b213aadec9503f8e960c31ded3f54ec07e9
shreya.shah <shreya.s@zyapaar.com>
2023-03-16 18:53:03 +0530

    first commit

---
+++ src/services/helper.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { getToken } from "../auth";
+
+export const BASE_URL = "http://blog-app-db.cm9iaqtbaa2y.ap-south-1.rds.amazonaws.com/api/v1";
+// export const BASE_URL = "https://apis.lcwdblogs.online/api/v1";
+
+export const myAxios = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const privateAxios = axios.create({
+  baseURL: BASE_URL,
+});
+
+privateAxios.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers.common.Authorization = `Bearer ${token}`;
+      // console.log(config);
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
