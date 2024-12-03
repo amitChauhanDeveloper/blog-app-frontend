a4078721b4b0e1f0b4586e4797b6495c63e55d73
amit chauhan <email@tender247.com>
2024-12-03 10:51:31 +0530

    add render backend url

--- src/services/helper.js
+++ src/services/helper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../auth";
 
 //export const BASE_URL = "http://blog-app-db.cm9iaqtbaa2y.ap-south-1.rds.amazonaws.com/api/v1";
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://blog-app-apis-2o43.onrender.com/api/v1";
 // export const BASE_URL = "https://apis.lcwdblogs.online/api/v1";
 
 export const myAxios = axios.create({
